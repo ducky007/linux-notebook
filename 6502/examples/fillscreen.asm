7ed76c4c3e9159f89fa3c2981c8c339f27279226
neauoire <aliceffekt@gmail.com>
2020-01-01 20:19:37 -0500

    *

--- 6502/examples/fillscreen.asm
+++ 6502/examples/fillscreen.asm
@@ -1,18 +1,23 @@
-; Store $0200 at zero-page $00-$01
+LDA #$00
+STA $00
+LDA #$02
+STA $01
+LDY #0
+
+loop:
+  LDA $fe
+  STA ($00), y
+  INY
+  BEQ next
+  JMP loop
+
+reset:
   LDA #$00
-  STA $00
-  LDA #$02
   STA $01
-; Draw to screen
-  LDA #1       ; Set color to white
-  LDY #0       ; Set memory offset to $00
 
-Loop:
-  STA ($00), y ; Write to ($00)+Y
-  INY          ; Increment Y
-  BEQ Next     ; Branch if Y is zero
-  JMP Loop
-
-Next:
-  INC $01      ; Increment MSB of address in $00-$01
-  JMP Loop
+next:
+  INC $01
+  LDX $01
+  CPX #6
+  BEQ reset
+  JMP loop
--- 6502/examples/random.asm
+++ 6502/examples/random.asm
@@ -1,12 +1,12 @@
 loop: 
-  lda $fe       ; A=rnd
-  sta $00       ; ZP(0)=A
-  lda $fe
-  and #$3       ; A=A&3
-  clc           ; Clear carry
-  adc #$2       ; A+=2
-  sta $01       ; ZP(1)=A
-  lda $fe       ; A=rnd
-  ldy #$0       ; Y=0
-  sta ($00),y   ; ZP(0),ZP(1)=y
-  jmp loop
+  LDA $fe       ; A=rnd
+  STA $00       ; ZP(0)=A
+  LDA $fe
+  AND #$3       ; A=A&3
+  CLC           ; Clear carry
+  ADC #$2       ; A+=2
+  STA $01       ; ZP(1)=A
+  LDA $fe       ; A=rnd
+  LDY #$0       ; Y=0
+  STA ($00),y   ; ZP(0),ZP(1)=y
+  JMP loop
