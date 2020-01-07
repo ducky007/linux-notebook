78c8d72a3a33da99ef7e85def2639eac60b46663
neauoire <aliceffekt@gmail.com>
2020-01-07 15:34:48 -0500

    *

--- 6502/projects/asm6/base/src/cart.asm
+++ 6502/projects/asm6/base/src/cart.asm
@@ -1,56 +1,11 @@
 include "src/head.asm"
     
-    ;  INIT code goes here
-
-LoadPalettes:
-  LDA $2002    ; read PPU status to reset the high/low latch
-  LDA #$3F
-  STA $2006    ; write the high byte of $3F00 address
-  LDA #$00
-  STA $2006    ; write the low byte of $3F00 address
-  LDX #$00
-LoadPalettesLoop:
-  LDA palette, x        ;load palette byte
-  STA $2007             ;write to PPU
-  INX                   ;set index to next byte
-  CPX #$20            
-  BNE LoadPalettesLoop  ;if x = $20, 32 bytes copied, all done
-
-
-
-  LDA #$80
-  STA $0200        ; put sprite 0 in center ($80) of screen vert
-  STA $0203        ; put sprite 0 in center ($80) of screen horiz
-  LDA #$00
-  STA $0201        ; tile number = 0
-  STA $0202        ; color = 0, no flipping
-
-  LDA #%10000000   ; enable NMI, sprites from Pattern Table 0
-  STA $2000
-
-  LDA #%00010000   ; enable sprites
-  STA $2001
-
-Forever:
-  JMP Forever     ;jump back to Forever, infinite loop
+include "src/main.asm"
 
 include "src/nmi.asm"
 
 include "src/tables.asm"
 
-;;;;;;;;;;;;;;;;;;;
-;;;   VECTORS   ;;;
-;;;;;;;;;;;;;;;;;;;
-
-    .pad $FFFA
-
-    .dw NMI
-    .dw RESET
-    .dw 0
-
-;;;;;;;;;;;;;;;;;;;
-;;;   CHR-ROM   ;;;
-;;;;;;;;;;;;;;;;;;;
+include "src/vectors.asm"
 
     .incbin "src/sprite.chr"
-
