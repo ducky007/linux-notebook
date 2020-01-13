13558aeaca61f859e874d0deac8140eb8fd0c570
neauoire <aliceffekt@gmail.com>
2020-01-13 17:27:34 -0500

    *

---
+++ 6502/projects/asm6/pong2/src/main.asm
@@ -0,0 +1,334 @@
+LoadPalettes:
+  LDA $2002             ; read PPU status to reset the high/low latch
+  LDA #$3F
+  STA $2006             ; write the high byte of $3F00 address
+  LDA #$00
+  STA $2006             ; write the low byte of $3F00 address
+  LDX #$00              ; start out at 0
+LoadPalettesLoop:
+  LDA palette, x        ; load data from address (palette + the value in x)
+                          ; 1st time through loop it will load palette+0
+                          ; 2nd time through loop it will load palette+1
+                          ; 3rd time through loop it will load palette+2
+                          ; etc
+  STA $2007             ; write to PPU
+  INX                   ; X = X + 1
+  CPX #$20              ; Compare X to hex $10, decimal 16 - copying 16 bytes = 4 sprites
+  BNE LoadPalettesLoop  ; Branch to LoadPalettesLoop if compare was Not Equal to zero
+                        ; if compare was equal to 32, keep going down
+
+
+  
+
+
+;;;Set some initial ball stats
+  LDA #$01
+  STA balldown
+  STA ballright
+  LDA #$00
+  STA ballup
+  STA ballleft
+  
+  LDA #$50
+  STA bally
+  
+  LDA #$80
+  STA ballx
+  
+  LDA #$02
+  STA ballspeedx
+  STA ballspeedy
+
+
+;;;Set initial score value
+  LDA #$00
+  STA scoreOnes
+  STA scoreTens
+  STA scoreHundreds
+
+
+;;:Set starting game state
+  LDA #STATEPLAYING
+  STA gamestate
+
+
+              
+  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+  STA $2000
+
+  LDA #%00011110   ; enable sprites, enable background, no clipping on left side
+  STA $2001
+
+Forever:
+  JMP Forever     ;jump back to Forever, infinite loop, waiting for NMI
+  
+ 
+
+NMI:
+  LDA #$00
+  STA $2003       ; set the low byte (00) of the RAM address
+  LDA #$02
+  STA $4014       ; set the high byte (02) of the RAM address, start the transfer
+
+  JSR DrawScore
+
+  ;;This is the PPU clean up section, so rendering the next frame starts properly.
+  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
+  STA $2000
+  LDA #%00011110   ; enable sprites, enable background, no clipping on left side
+  STA $2001
+  LDA #$00        ;;tell the ppu there is no background scrolling
+  STA $2005
+  STA $2005
+    
+  ;;;all graphics updates done by here, run game engine
+
+
+  JSR ReadController1  ;;get the current button data for player 1
+  JSR ReadController2  ;;get the current button data for player 2
+  
+GameEngine:  
+  LDA gamestate
+  CMP #STATETITLE
+  BEQ EngineTitle    ;;game is displaying title screen
+    
+  LDA gamestate
+  CMP #STATEGAMEOVER
+  BEQ EngineGameOver  ;;game is displaying ending screen
+  
+  LDA gamestate
+  CMP #STATEPLAYING
+  BEQ EnginePlaying   ;;game is playing
+GameEngineDone:  
+  
+  JSR UpdateSprites  ;;set ball/paddle sprites from positions
+
+  RTI             ; return from interrupt
+ 
+ 
+ 
+ 
+;;;;;;;;
+ 
+EngineTitle:
+  ;;if start button pressed
+  ;;  turn screen off
+  ;;  load game screen
+  ;;  set starting paddle/ball position
+  ;;  go to Playing State
+  ;;  turn screen on
+  JMP GameEngineDone
+
+;;;;;;;;; 
+ 
+EngineGameOver:
+  ;;if start button pressed
+  ;;  turn screen off
+  ;;  load title screen
+  ;;  go to Title State
+  ;;  turn screen on 
+  JMP GameEngineDone
+ 
+;;;;;;;;;;;
+ 
+EnginePlaying:
+
+MoveBallRight:
+  LDA ballright
+  BEQ MoveBallRightDone   ;;if ballright=0, skip this section
+
+  LDA ballx
+  CLC
+  ADC ballspeedx        ;;ballx position = ballx + ballspeedx
+  STA ballx
+
+  LDA ballx
+  CMP #RIGHTWALL
+  BCC MoveBallRightDone      ;;if ball x < right wall, still on screen, skip next section
+  LDA #$00
+  STA ballright
+  LDA #$01
+  STA ballleft         ;;bounce, ball now moving left
+  ;;in real game, give point to player 1, reset ball
+  jsr IncrementScore
+MoveBallRightDone:
+
+
+MoveBallLeft:
+  LDA ballleft
+  BEQ MoveBallLeftDone   ;;if ballleft=0, skip this section
+
+  LDA ballx
+  SEC
+  SBC ballspeedx        ;;ballx position = ballx - ballspeedx
+  STA ballx
+
+  LDA ballx
+  CMP #LEFTWALL
+  BCS MoveBallLeftDone      ;;if ball x > left wall, still on screen, skip next section
+  LDA #$01
+  STA ballright
+  LDA #$00
+  STA ballleft         ;;bounce, ball now moving right
+  ;;in real game, give point to player 2, reset ball
+  jsr IncrementScore
+MoveBallLeftDone:
+
+
+MoveBallUp:
+  LDA ballup
+  BEQ MoveBallUpDone   ;;if ballup=0, skip this section
+
+  LDA bally
+  SEC
+  SBC ballspeedy        ;;bally position = bally - ballspeedy
+  STA bally
+
+  LDA bally
+  CMP #TOPWALL
+  BCS MoveBallUpDone      ;;if ball y > top wall, still on screen, skip next section
+  LDA #$01
+  STA balldown
+  LDA #$00
+  STA ballup         ;;bounce, ball now moving down
+MoveBallUpDone:
+
+
+MoveBallDown:
+  LDA balldown
+  BEQ MoveBallDownDone   ;;if ballup=0, skip this section
+
+  LDA bally
+  CLC
+  ADC ballspeedy        ;;bally position = bally + ballspeedy
+  STA bally
+
+  LDA bally
+  CMP #BOTTOMWALL
+  BCC MoveBallDownDone      ;;if ball y < bottom wall, still on screen, skip next section
+  LDA #$00
+  STA balldown
+  LDA #$01
+  STA ballup         ;;bounce, ball now moving down
+MoveBallDownDone:
+
+MovePaddleUp:
+  ;;if up button pressed
+  ;;  if paddle top > top wall
+  ;;    move paddle top and bottom up
+MovePaddleUpDone:
+
+MovePaddleDown:
+  ;;if down button pressed
+  ;;  if paddle bottom < bottom wall
+  ;;    move paddle top and bottom down
+MovePaddleDownDone:
+  
+CheckPaddleCollision:
+  ;;if ball x < paddle1x
+  ;;  if ball y > paddle y top
+  ;;    if ball y < paddle y bottom
+  ;;      bounce, ball now moving left
+CheckPaddleCollisionDone:
+
+  JMP GameEngineDone
+ 
+ 
+ 
+ 
+UpdateSprites:
+  LDA bally  ;;update all ball sprite info
+  STA $0200
+  
+  LDA #$30
+  STA $0201
+  
+  LDA #$00
+  STA $0202
+  
+  LDA ballx
+  STA $0203
+  
+  ;;update paddle sprites
+  RTS
+ 
+ 
+DrawScore:
+  LDA $2002
+  LDA #$20
+  STA $2006
+  LDA #$20
+  STA $2006          ; start drawing the score at PPU $2020
+  
+  LDA scoreHundreds  ; get first digit
+;  CLC
+;  ADC #$30           ; add ascii offset  (this is UNUSED because the tiles for digits start at 0)
+  STA $2007          ; draw to background
+  LDA scoreTens      ; next digit
+;  CLC
+;  ADC #$30           ; add ascii offset
+  STA $2007
+  LDA scoreOnes      ; last digit
+;  CLC
+;  ADC #$30           ; add ascii offset
+  STA $2007
+  RTS
+ 
+ 
+IncrementScore:
+IncOnes:
+  LDA scoreOnes      ; load the lowest digit of the number
+  CLC 
+  ADC #$01           ; add one
+  STA scoreOnes
+  CMP #$0A           ; check if it overflowed, now equals 10
+  BNE IncDone        ; if there was no overflow, all done
+IncTens:
+  LDA #$00
+  STA scoreOnes      ; wrap digit to 0
+  LDA scoreTens      ; load the next digit
+  CLC 
+  ADC #$01           ; add one, the carry from previous digit
+  STA scoreTens
+  CMP #$0A           ; check if it overflowed, now equals 10
+  BNE IncDone        ; if there was no overflow, all done
+IncHundreds:
+  LDA #$00
+  STA scoreTens      ; wrap digit to 0
+  LDA scoreHundreds  ; load the next digit
+  CLC 
+  ADC #$01           ; add one, the carry from previous digit
+  STA scoreHundreds
+IncDone:
+
+
+
+
+ 
+ReadController1:
+  LDA #$01
+  STA $4016
+  LDA #$00
+  STA $4016
+  LDX #$08
+ReadController1Loop:
+  LDA $4016
+  LSR A            ; bit0 -> Carry
+  ROL buttons1     ; bit0 <- Carry
+  DEX
+  BNE ReadController1Loop
+  RTS
+  
+ReadController2:
+  LDA #$01
+  STA $4016
+  LDA #$00
+  STA $4016
+  LDX #$08
+ReadController2Loop:
+  LDA $4017
+  LSR A            ; bit0 -> Carry
+  ROL buttons2     ; bit0 <- Carry
+  DEX
+  BNE ReadController2Loop
+  RTS  
