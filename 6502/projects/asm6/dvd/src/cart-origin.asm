ee22872000d368b3c26879c48db71ab872accb97
neauoire <aliceffekt@gmail.com>
2020-01-13 22:37:22 -0500

    *

--- 6502/projects/asm6/dvd/src/cart-origin.asm
+++ 6502/projects/asm6/dvd/src/cart-origin.asm
@@ -1,3 +1,86 @@
+  .inesprg 1   ; 1x 16KB PRG code
+  .ineschr 1   ; 1x  8KB CHR data
+  .inesmap 0   ; mapper 0 = NROM, no bank swapping
+  .inesmir 1   ; background mirroring
+  
+
+;;;;;;;;;;;;;;;
+
+;; DECLARE SOME VARIABLES HERE
+  .rsset $0000  ;;start variables at ram location 0
+  
+gamestate  .rs 1  ; .rs 1 means reserve one byte of space
+ballx      .rs 1  ; ball horizontal position
+bally      .rs 1  ; ball vertical position
+ballup     .rs 1  ; 1 = ball moving up
+balldown   .rs 1  ; 1 = ball moving down
+ballleft   .rs 1  ; 1 = ball moving left
+ballright  .rs 1  ; 1 = ball moving right
+ballspeedx .rs 1  ; ball horizontal speed per frame
+ballspeedy .rs 1  ; ball vertical speed per frame
+paddle1ytop   .rs 1  ; player 1 paddle top vertical position
+paddle2ybot   .rs 1  ; player 2 paddle bottom vertical position
+buttons1   .rs 1  ; player 1 gamepad buttons, one bit per button
+buttons2   .rs 1  ; player 2 gamepad buttons, one bit per button
+score1     .rs 1  ; player 1 score, 0-15
+score2     .rs 1  ; player 2 score, 0-15
+
+
+;; DECLARE SOME CONSTANTS HERE
+STATETITLE     = $00  ; displaying title screen
+STATEPLAYING   = $01  ; move paddles/ball, check for collisions
+STATEGAMEOVER  = $02  ; displaying game over screen
+  
+RIGHTWALL      = $F4  ; when ball reaches one of these, do something
+TOPWALL        = $20
+BOTTOMWALL     = $E0
+LEFTWALL       = $04
+  
+PADDLE1X       = $08  ; horizontal position for paddles, doesnt move
+PADDLE2X       = $F0
+
+;;;;;;;;;;;;;;;;;;
+
+
+
+
+  .bank 0
+  .org $C000 
+RESET:
+  SEI          ; disable IRQs
+  CLD          ; disable decimal mode
+  LDX #$40
+  STX $4017    ; disable APU frame IRQ
+  LDX #$FF
+  TXS          ; Set up stack
+  INX          ; now X = 0
+  STX $2000    ; disable NMI
+  STX $2001    ; disable rendering
+  STX $4010    ; disable DMC IRQs
+
+vblankwait1:       ; First wait for vblank to make sure PPU is ready
+  BIT $2002
+  BPL vblankwait1
+
+clrmem:
+  LDA #$00
+  STA $0000, x
+  STA $0100, x
+  STA $0300, x
+  STA $0400, x
+  STA $0500, x
+  STA $0600, x
+  STA $0700, x
+  LDA #$FE
+  STA $0200, x
+  INX
+  BNE clrmem
+   
+vblankwait2:      ; Second wait for vblank, PPU is ready after this
+  BIT $2002
+  BPL vblankwait2
+
+
 LoadPalettes:
   LDA $2002             ; read PPU status to reset the high/low latch
   LDA #$3F
@@ -7,14 +90,21 @@ LoadPalettes:
   LDX #$00              ; start out at 0
 LoadPalettesLoop:
   LDA palette, x        ; load data from address (palette + the value in x)
+                          ; 1st time through loop it will load palette+0
+                          ; 2nd time through loop it will load palette+1
+                          ; 3rd time through loop it will load palette+2
+                          ; etc
   STA $2007             ; write to PPU
   INX                   ; X = X + 1
   CPX #$20              ; Compare X to hex $10, decimal 16 - copying 16 bytes = 4 sprites
   BNE LoadPalettesLoop  ; Branch to LoadPalettesLoop if compare was Not Equal to zero
                         ; if compare was equal to 32, keep going down
 
-; Set some initial ball stats
 
+  
+
+
+;;;Set some initial ball stats
   LDA #$01
   STA balldown
   STA ballright
@@ -28,15 +118,17 @@ LoadPalettesLoop:
   LDA #$80
   STA ballx
   
-  LDA #$01
+  LDA #$02
   STA ballspeedx
   STA ballspeedy
 
-; Set starting game state
 
+;;:Set starting game state
   LDA #STATEPLAYING
   STA gamestate
-       
+
+
+              
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
   STA $2000
 
@@ -44,49 +136,56 @@ LoadPalettesLoop:
   STA $2001
 
 Forever:
-  JMP Forever      ; jump back to Forever, infinite loop, waiting for NMI
+  JMP Forever     ;jump back to Forever, infinite loop, waiting for NMI
   
+ 
+
 NMI:
   LDA #$00
-  STA $2003        ; set the low byte (00) of the RAM address
+  STA $2003       ; set the low byte (00) of the RAM address
   LDA #$02
-  STA $4014        ; set the high byte (02) of the RAM address, start the transfer
+  STA $4014       ; set the high byte (02) of the RAM address, start the transfer
 
   JSR DrawScore
 
-  ; This is the PPU clean up section, so rendering the next frame starts properly.
-
+  ;;This is the PPU clean up section, so rendering the next frame starts properly.
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
   STA $2000
   LDA #%00011110   ; enable sprites, enable background, no clipping on left side
   STA $2001
-  LDA #$00         ; tell the ppu there is no background scrolling
+  LDA #$00        ;;tell the ppu there is no background scrolling
   STA $2005
   STA $2005
     
-  ; all graphics updates done by here, run game engin 
+  ;;;all graphics updates done by here, run game engine
 
-  JSR ReadController1  ; get the current button data for player 1
-  JSR ReadController2  ; get the current button data for player 2
+
+  JSR ReadController1  ;;get the current button data for player 1
+  JSR ReadController2  ;;get the current button data for player 2
   
 GameEngine:  
   LDA gamestate
   CMP #STATETITLE
-  BEQ EngineTitle      ; game is displaying title screen
+  BEQ EngineTitle    ;;game is displaying title screen
     
   LDA gamestate
   CMP #STATEGAMEOVER
-  BEQ EngineGameOver   ; game is displaying ending screen
+  BEQ EngineGameOver  ;;game is displaying ending screen
   
   LDA gamestate
   CMP #STATEPLAYING
-  BEQ EnginePlaying    ; game is playing
+  BEQ EnginePlaying   ;;game is playing
 GameEngineDone:  
-  JSR UpdateSprites    ; set ball/paddle sprites from positions
-  RTI                  ; return from interrupt
- 
-;
+  
+  JSR UpdateSprites  ;;set ball/paddle sprites from positions
 
+  RTI             ; return from interrupt
+ 
+ 
+ 
+ 
+;;;;;;;;
+ 
 EngineTitle:
   ;;if start button pressed
   ;;  turn screen off
@@ -96,7 +195,7 @@ EngineTitle:
   ;;  turn screen on
   JMP GameEngineDone
 
-; 
+;;;;;;;;; 
  
 EngineGameOver:
   ;;if start button pressed
@@ -106,7 +205,7 @@ EngineGameOver:
   ;;  turn screen on 
   JMP GameEngineDone
  
-; 
+;;;;;;;;;;;
  
 EnginePlaying:
 
@@ -208,155 +307,32 @@ CheckPaddleCollisionDone:
 
   JMP GameEngineDone
  
-; 
-
+ 
+ 
+ 
 UpdateSprites:
-  LDA bally  ; update all ball sprite info
+  LDA bally  ;;update all ball sprite info
   STA $0200
-  LDA #$02   ; sprite tile
+  
+  LDA #$30
   STA $0201
-  LDA #$00   ; 
+  
+  LDA #$00
   STA $0202
+  
   LDA ballx
   STA $0203
-
-  ; ball 2
-  LDA bally  ; update all ball sprite info
-  STA $0204
-  LDA #$03   ; sprite tile
-  STA $0205
-  LDA #$00   ; palette
-  STA $0206
-  LDA ballx
-  ADC #$08
-  STA $0207
-
-  ; ball 3
-  LDA bally  ; update all ball sprite info
-  STA $0208
-  LDA #$04   ; sprite tile
-  STA $0209
-  LDA #$00   ; 
-  STA $020a
-  LDA ballx
-  ADC #$10
-  STA $020b
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  STA $020c
-  LDA #$05   ; sprite tile
-  STA $020d
-  LDA #$00   ; 
-  STA $020e
-  LDA ballx
-  ADC #$18
-  STA $020f
-
-  ; row 2
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #08
-  STA $0210
-  LDA #$12   ; sprite tile
-  STA $0211
-  LDA #$00   ; 
-  STA $0212
-  LDA ballx
-  STA $0213
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #08
-  STA $0214
-  LDA #$13   ; sprite tile
-  STA $0215
-  LDA #$00   ; 
-  STA $0216
-  LDA ballx
-  ADC #$08
-  STA $0217
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #08
-  STA $0218
-  LDA #$14   ; sprite tile
-  STA $0219
-  LDA #$00   ; 
-  STA $021a
-  LDA ballx
-  ADC #$10
-  STA $021b
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #08
-  STA $021c
-  LDA #$15   ; sprite tile
-  STA $021d
-  LDA #$00   ; 
-  STA $021e
-  LDA ballx
-  ADC #$18
-  STA $021f
-
-  ; row 3
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #$10
-  STA $0220
-  LDA #$22   ; sprite tile
-  STA $0221
-  LDA #$00   ; 
-  STA $0222
-  LDA ballx
-  STA $0223
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #$10
-  STA $0224
-  LDA #$23   ; sprite tile
-  STA $0225
-  LDA #$00   ; 
-  STA $0226
-  LDA ballx
-  ADC #$08
-  STA $0227
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #$10
-  STA $0228
-  LDA #$24   ; sprite tile
-  STA $0229
-  LDA #$00   ; 
-  STA $022a
-  LDA ballx
-  ADC #$10
-  STA $022b
-
-  ; ball 4
-  LDA bally  ; update all ball sprite info
-  ADC #$10
-  STA $022c
-  LDA #$25   ; sprite tile
-  STA $022d
-  LDA #$00   ; 
-  STA $022e
-  LDA ballx
-  ADC #$18
-  STA $022f
-
+  
+  ;;update paddle sprites
   RTS
+ 
  
 DrawScore:
   ;;draw score on screen using background tiles
   ;;or using many sprites
   RTS
+ 
+ 
  
 ReadController1:
   LDA #$01
@@ -384,4 +360,41 @@ ReadController2Loop:
   ROL buttons2     ; bit0 <- Carry
   DEX
   BNE ReadController2Loop
-  RTS 
+  RTS  
+  
+  
+    
+        
+;;;;;;;;;;;;;;  
+  
+  
+  
+  .bank 1
+  .org $E000
+palette:
+  .db $22,$29,$1A,$0F,  $22,$36,$17,$0F,  $22,$30,$21,$0F,  $22,$27,$17,$0F   ;;background palette
+  .db $22,$1C,$15,$14,  $22,$02,$38,$3C,  $22,$1C,$15,$14,  $22,$02,$38,$3C   ;;sprite palette
+
+sprites:
+     ;vert tile attr horiz
+  .db $80, $32, $00, $80   ;sprite 0
+  .db $80, $33, $00, $88   ;sprite 1
+  .db $88, $34, $00, $80   ;sprite 2
+  .db $88, $35, $00, $88   ;sprite 3
+
+
+
+  .org $FFFA     ;first of the three vectors starts here
+  .dw NMI        ;when an NMI happens (once per frame if enabled) the 
+                   ;processor will jump to the label NMI:
+  .dw RESET      ;when the processor first turns on or is reset, it will jump
+                   ;to the label RESET:
+  .dw 0          ;external interrupt IRQ is not used in this tutorial
+  
+  
+;;;;;;;;;;;;;;  
+  
+  
+  .bank 2
+  .org $0000
+  .incbin "mario.chr"   ;includes 8KB graphics file from SMB1
