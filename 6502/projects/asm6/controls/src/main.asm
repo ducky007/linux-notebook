2fc22f5acadf0f2ee10faf8ae5f4d61910f3c552
neauoire <aliceffekt@gmail.com>
2020-01-16 19:51:17 -0500

    *

--- 6502/projects/asm6/controls/src/main.asm
+++ 6502/projects/asm6/controls/src/main.asm
@@ -1,5 +1,3 @@
-    ;  INIT code goes here
-
 LoadPalettes:
   LDA $2002             ; read PPU status to reset the high/low latch
   LDA #$3F
@@ -19,8 +17,6 @@ LoadPalettesLoop:
   BNE LoadPalettesLoop  ; Branch to LoadPalettesLoop if compare was Not Equal to zero
                         ; if compare was equal to 32, keep going down
 
-
-
 LoadSprites:
   LDX #$00              ; start at 0
 LoadSpritesLoop:
@@ -30,8 +26,9 @@ LoadSpritesLoop:
   CPX #$20              ; Compare X to hex $20, decimal 32
   BNE LoadSpritesLoop   ; Branch to LoadSpritesLoop if compare was Not Equal to zero
                         ; if compare was equal to 32, keep going down
-              
-              
+  
+  LDA #$05   ; sprite tile
+  STA $0201
 
   LDA #%10000000   ; enable NMI, sprites from Pattern Table 1
   STA $2000
@@ -54,7 +51,6 @@ LatchController:
   LDA #$00
   STA $4016       ; tell both the controllers to latch buttons
 
-
 ReadA: 
   LDA $4016       ; player 1 - A
   AND #%00000001  ; only look at bit 0
@@ -64,20 +60,87 @@ ReadA:
   CLC             ; make sure the carry flag is clear
   ADC #$01        ; A = A + 1
   STA $0203       ; save sprite X position
+  LDA #$06        ; sprite tile
+  STA $0201
 ReadADone:        ; handling this button is done
   
-
 ReadB: 
   LDA $4016       ; player 1 - B
   AND #%00000001  ; only look at bit 0
   BEQ ReadBDone   ; branch to ReadBDone if button is NOT pressed (0)
                   ; add instructions here to do something when button IS pressed (1)
+  LDA #$06        ; sprite tile
+  STA $0201
+ReadBDone:        ; handling this button is done
+
+ReadSel: 
+  LDA $4016       ; player 1 - B
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadSelDone ; branch to ReadBDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
+  LDA #$06        ; sprite tile
+  STA $0201
+ReadSelDone:        ; handling this button is done
+
+ReadStart: 
+  LDA $4016       ; player 1 - B
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadStartDone ; branch to ReadBDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
+  LDA #$06        ; sprite tile
+  STA $0201
+ReadStartDone:        ; handling this button is done
+
+ReadUp: 
+  LDA $4016       ; player 1 - B
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadUpDone ; branch to ReadBDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
+  LDA $0200       ; load sprite X position
+  SEC             ; make sure carry flag is set
+  SBC #$01        ; A = A - 1
+  STA $0200       ; save sprite X position
+  LDA #$01        ; sprite tile
+  STA $0201
+ReadUpDone:        ; handling this button is done
+
+ReadDown: 
+  LDA $4016       ; player 1 - B
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadDownDone ; branch to ReadBDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
+  LDA $0200       ; load sprite X position
+  SEC             ; make sure carry flag is set
+  ADC #$01        ; A = A - 1
+  STA $0200       ; save sprite X position
+  LDA #$02        ; sprite tile
+  STA $0201
+ReadDownDone:        ; handling this button is done
+
+ReadLeft: 
+  LDA $4016       ; player 1 - B
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadLeftDone ; branch to ReadBDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
   LDA $0203       ; load sprite X position
   SEC             ; make sure carry flag is set
   SBC #$01        ; A = A - 1
   STA $0203       ; save sprite X position
-ReadBDone:        ; handling this button is done
+  LDA #$03        ; sprite tile
+  STA $0201
+ReadLeftDone:        ; handling this button is done
 
-
+ReadRight: 
+  LDA $4016       ; player 1 - B
+  AND #%00000001  ; only look at bit 0
+  BEQ ReadRightDone ; branch to ReadBDone if button is NOT pressed (0)
+                  ; add instructions here to do something when button IS pressed (1)
+  LDA $0203       ; load sprite X position
+  SEC             ; make sure carry flag is set
+  ADC #$01        ; A = A - 1
+  STA $0203       ; save sprite X position
+  LDA #$04        ; sprite tile
+  STA $0201
+ReadRightDone:        ; handling this button is done
   
   RTI             ; return from interrupt
