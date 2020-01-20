b0f15f04a19f99da4407c3e5963fc721b8e2e133
neauoire <aliceffekt@gmail.com>
2020-01-20 17:15:51 -0500

    Added controller example project

--- 6502/projects/basics/gui/src/main.asm
+++ 6502/projects/basics/gui/src/main.asm
@@ -22,7 +22,7 @@ LoadPalettesLoop:
 
 ; sprite
 
-DrawSprite1:
+DrawBit1:
   LDA #$0f
   STA $0200        ; set tile.y pos
   LDA #$01
@@ -32,7 +32,7 @@ DrawSprite1:
   LDA #$08
   STA $0203        ; set tile.x pos
 
-DrawSprite2:
+DrawBit2:
   LDA #$0f
   STA $0204        ; set tile.y pos
   LDA #$02
@@ -42,7 +42,7 @@ DrawSprite2:
   LDA #$10
   STA $0207        ; set tile.x pos
 
-DrawSprite3:
+DrawBit3:
   LDA #$0f
   STA $0208        ; set tile.y pos
   LDA #$03
@@ -52,7 +52,7 @@ DrawSprite3:
   LDA #$18
   STA $020b        ; set tile.x pos
 
-DrawSprite4:
+DrawBit4:
   LDA #$0f
   STA $020c        ; set tile.y pos
   LDA #$03
@@ -62,7 +62,7 @@ DrawSprite4:
   LDA #$20
   STA $020f        ; set tile.x pos
 
-DrawSprite5:
+DrawBit5:
   LDA #$0f
   STA $0210        ; set tile.y pos
   LDA #$03
@@ -72,7 +72,7 @@ DrawSprite5:
   LDA #$28
   STA $0213        ; set tile.x pos
 
-DrawSprite6:
+DrawBit6:
   LDA #$0f
   STA $0214        ; set tile.y pos
   LDA #$03
@@ -81,38 +81,6 @@ DrawSprite6:
   STA $0216        ; set tile.attribute
   LDA #$30
   STA $0217        ; set tile.x pos
-
-DrawHealth:
-  LDA #$0f
-  STA $0218        ; set tile.y pos
-  LDA health
-  STA $0219        ; set tile.id
-  LDA #$00
-  STA $021a        ; set tile.attribute
-  LDA #$40
-  STA $021b        ; set tile.x pos
-
-; background
-
-LoadBackground:
-  LDA $2002             ; read PPU status to reset the high/low latch
-  LDA #$20
-  STA $2006             ; write the high byte of $2000 address
-  LDA #$00
-  STA $2006             ; write the low byte of $2000 address
-  LDX #$00              ; start out at 0
-
-; screen segment 1/4
-
-LoadBackgroundLoop:
-  LDA background, x     ; load data from address (background + the value in x)
-  STA $2007             ; write to PPU
-  INX                   ; X = X + 1
-  CPX #$00              ; Each background table row is $10 in length
-  BNE LoadBackgroundLoop  ; Branch to LoadBackgroundLoop if compare was Not Equal to zero
-
-  LDX #$00
-  JSR UpdateBar
 
 EnableSprites:
   LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
@@ -129,16 +97,6 @@ NMI:
   LDA #$02
   STA $4014       ; set the high byte (02) of the RAM address, start the transfer
 
-  ;;This is the PPU clean up section, so rendering the next frame starts properly.
-
-  LDA #%10010000   ; enable NMI, sprites from Pattern Table 0, background from Pattern Table 1
-  STA $2000
-  LDA #%00011110   ; enable sprites, enable background, no clipping on left side
-  STA $2001
-  LDA #$00        ;;tell the ppu there is no background scrolling
-  STA $2005
-  STA $2005
-
 LatchController:
   LDA #$01
   STA $4016
@@ -147,71 +105,67 @@ LatchController:
 
 ReadA: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadADone
-  LDA #$06        ; sprite tile
-  STA $0201
-ReadADone:        ; handling this button is done
+  NOP             ; Do nothing..
+ReadADone:
   
 ReadB: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadBDone
-  LDA #$06        ; sprite tile
-  STA $0201
-ReadBDone:        ; handling this button is done
+  NOP             ; Do nothing..
+ReadBDone:
 
 ReadSel: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadSelDone 
-  LDA #$06        ; sprite tile
-  STA $0201
-ReadSelDone:        ; handling this button is done
+  NOP             ; Do nothing..
+ReadSelDone:
 
 ReadStart: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadStartDone 
-  LDA #$06        ; sprite tile
-  STA $0201
-ReadStartDone:        ; handling this button is done
+  NOP             ; Do nothing..
+ReadStartDone:
 
 ReadUp: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadUpDone 
   JSR IncreaseHealth
   JSR UpdateHealth
   JSR UpdateBar
-ReadUpDone:        ; handling this button is done
+ReadUpDone:
 
 ReadDown: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadDownDone 
   JSR DecreaseHealth
   JSR UpdateHealth
   JSR UpdateBar
-ReadDownDone:        ; handling this button is done
+ReadDownDone:
 
 ReadLeft: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadLeftDone 
-  DEC $0203
-  LDA #$03        ; sprite tile
-  STA $0201
-ReadLeftDone:        ; handling this button is done
+  JSR DecreaseHealth
+  JSR UpdateHealth
+  JSR UpdateBar
+ReadLeftDone:
 
 ReadRight: 
   LDA $4016
-  AND #%00000001  ; only look at bit 0
+  AND #%00000001
   BEQ ReadRightDone 
-  INC $0203
-  LDA #$04        ; sprite tile
-  STA $0201
-ReadRightDone:        ; handling this button is done
+  JSR IncreaseHealth
+  JSR UpdateHealth
+  JSR UpdateBar
+ReadRightDone:
   
   RTI             ; return from interrupt
 
